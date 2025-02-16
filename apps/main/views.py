286275bc66d53213b614632f5a76438aa2555f1b
Just_NoWorking322 <alisherbolotbekov03@gmail.com>
2025-02-16 15:07:47 +0600

    third commit

--- apps/main/views.py
+++ apps/main/views.py
@@ -2,9 +2,9 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 from apps.main.models import Settings, Products, Characteristic
 from apps.main.serializer import  ProductSerializer, CharacteristicSerializer
-
-
 from apps.main.pagination import ProducPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductMixins(GenericViewSet,
                     mixins.CreateModelMixin,
@@ -17,7 +17,12 @@ class ProductMixins(GenericViewSet,
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProducPagination
-
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    
+    search_fields = ['title','description']
+    ordering_fields = ['price', 'created_app']
+    ordering = ['=created_att']
+    
 class CharacteristicViewSet(GenericViewSet,
                     mixins.CreateModelMixin,
                     mixins.ListModelMixin,
